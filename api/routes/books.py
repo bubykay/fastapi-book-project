3b7d951ff8d350336194cda3f1d4d469dc03853a
Kayode <bubykay@gmail.com>
2025-02-11 06:28:49 +0100

    test changes

--- api/routes/books.py
+++ api/routes/books.py
@@ -56,7 +56,7 @@ async def update_book(book_id: int, book: Book) -> Book:
         content=db.update_book(book_id, book).model_dump(),
     )
     
-@router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
+@router.get("/{book_id}", status_code=status.HTTP_200_OK)
 async def get_book(book_id: int):
     book = db.get_book(book_id)
     if not book:
@@ -64,7 +64,7 @@ async def get_book(book_id: int):
             status_code=status.HTTP_404_NOT_FOUND, detail="Book not found"
         )
     
-    return book
+    return "hello world"
 
 
 @router.delete("/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
